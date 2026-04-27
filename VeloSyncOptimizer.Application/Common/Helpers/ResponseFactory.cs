a82cbae88e79a6241f0fb63ca29dca06ac22c520
majid <majidvp501@gmail.com>
2026-04-27 21:36:23 +0530

    Added(auth): Commmon Response for the Apis and Exception Middleware

---
+++ VeloSyncOptimizer.Application/Common/Helpers/ResponseFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeloSyncOptimizer.Application.Common.Models;
+
+namespace VeloSyncOptimizer.Application.Common.Helpers;
+
+public static class ResponseFactory
+{
+    public static ApiResponse<T> Success<T>(T data, string message = "Success")
+    {
+        return new ApiResponse<T>
+        {
+            Success = true,
+            Message = message,
+            Data = data
+        };
+    }
+
+    public static ApiResponse<T> Failure<T>(string message, List<string>? errors = null)
+    {
+        return new ApiResponse<T>
+        {
+            Success = false,
+            Message = message,
+            Errors = errors ?? new List<string>()
+        };
+    }
+}
