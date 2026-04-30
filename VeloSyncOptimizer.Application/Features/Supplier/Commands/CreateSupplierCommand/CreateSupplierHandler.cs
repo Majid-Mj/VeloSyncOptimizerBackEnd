01fa360837fba6e5965915088dc36c0042738e20
majid <majidvp501@gmail.com>
2026-04-30 18:44:59 +0530

    feat(supplier): Create Supplier Handler implimented

---
+++ VeloSyncOptimizer.Application/Features/Supplier/Commands/CreateSupplierCommand/CreateSupplierHandler.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using VeloSyncOptimizer.Application.Common.Interfaces.Repositories;
+using VeloSyncOptimizer.Application.Common.Interfaces.Services;
+using VeloSyncOptimizer.Domain.Entities;
+
+public class CreateSupplierHandler
+    : IRequestHandler<CreateSupplierCommand, Guid>
+{
+    private readonly IGenericRepository<Supplier> _repo;
+    private readonly ICategoryRepository _queryRepo; // Dapper (for duplicate check)
+    private readonly ICacheService _cache;
+
+    public CreateSupplierHandler(
+        IGenericRepository<Supplier> repo,
+        ICategoryRepository queryRepo,
+        ICacheService cache)
+    {
+        _repo = repo;
+        _queryRepo = queryRepo;
+        _cache = cache;
+    }
+
+    public async Task<Guid> Handle(CreateSupplierCommand request, CancellationToken ct)
+    {
+        // Duplicate check (by Name)
+        var existing = (await _queryRepo.QueryAsync<Supplier>(
+            "inventory.sp_CheckSupplierExists",
+            new { request.Name }))
+            .FirstOrDefault();
+
+        if (existing != null)
+            throw new Exception("Supplier with this name already exists");
+
+        // Create entity
+        var supplier = new Supplier
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name.Trim(),
+            ContactEmail = request.ContactEmail,
+            ContactPhone = request.ContactPhone,
+            CreatedAt = DateTime.UtcNow,
+            IsActive = true
+        };
+
+        // Save
+        await _repo.AddAsync(supplier, ct);
+        await _repo.SaveChangesAsync(ct);
+
+        //Cache invalidation
+        await _cache.RemoveAsync("suppliers:all");
+
+        return supplier.Id;
+    }
+}
